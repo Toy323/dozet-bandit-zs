e6560c941e55c35cc899f9d5306f1244ebed39f5
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-12-11 17:50:20 +0900

    bl

--- entities/weapons/weapon_zs_sprayersmg.lua
+++ entities/weapons/weapon_zs_sprayersmg.lua
@@ -26,9 +26,9 @@ SWEP.UseHands = true
 SWEP.Primary.Sound = Sound("Weapon_MAC10.Single")
 SWEP.Primary.Damage = 12
 SWEP.Primary.NumShots = 1
-SWEP.Primary.Delay = 0.057
+SWEP.Primary.Delay = 0.075
 
-SWEP.Primary.ClipSize = 40
+SWEP.Primary.ClipSize = 32
 SWEP.Primary.Automatic = true
 SWEP.Primary.Ammo = "smg1"
 
@@ -37,8 +37,8 @@ GAMEMODE:SetupDefaultClip(SWEP.Primary)
 SWEP.Primary.Gesture = ACT_HL2MP_GESTURE_RANGE_ATTACK_SMG1
 SWEP.ReloadGesture = ACT_HL2MP_GESTURE_RELOAD_SMG1
 
-SWEP.ConeMax = 0.13
-SWEP.ConeMin = 0.025
+SWEP.ConeMax = 0.19
+SWEP.ConeMin = 0.055
 SWEP.MovingConeOffset = -0.02
 SWEP.Recoil = 0.45
 GAMEMODE:SetupAimDefaults(SWEP,SWEP.Primary)
